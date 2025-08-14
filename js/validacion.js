a2fb8d72fff6d1b085bc0544147a37f612be08db
Nachoofiore <Nachoofiore@hotmail.com>
2025-08-14 19:17:42 -0300

    Update validacion.js

--- js/validacion.js
+++ js/validacion.js
@@ -1,7 +1,58 @@
-function showAlertSuccess() {
-    document.getElementById("alert-success").classList.add("show");
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const registerBtn = document.getElementById('regBtn');
+    const nombre = document.getElementById('nombre');
+    const apellido = document.getElementById('apellido');
+    const email = document.getElementById('email');
+    const password1 = document.getElementById('password1');
+    const password2 = document.getElementById('password2');
+    const terminos = document.getElementById('terminos');
 
-function showAlertError() {
-    document.getElementById("alert-danger").classList.add("show");
-}
+    function validarFormulario() {
+        if (nombre.value.trim() === '' || 
+            apellido.value.trim() === '' || 
+            email.value.trim() === '' || 
+            password1.value.trim() === '' || 
+            password2.value.trim() === '') {
+            return false;
+        }
+
+        if (password1.value.length < 6) {
+            return false;
+        }
+
+        if (password1.value !== password2.value) {
+            return false;
+        }
+
+        if (!terminos.checked) {
+            return false;
+        }
+        else {
+        return true;
+        }
+    }
+
+    function showAlertSuccess() {
+        const alertSuccess = document.getElementById("alert-success");
+        alertSuccess.classList.add('show');
+        setTimeout(() => {
+            alertSuccess.classList.remove('show');
+        }, 3000);
+    }
+
+    function showAlertError() {
+        const alertError = document.getElementById("alert-danger");
+        alertError.classList.add('show');
+        setTimeout(() => {
+            alertError.classList.remove('show');
+        }, 3000);
+    }
+
+    registerBtn.addEventListener('click', function() {
+        if (validarFormulario()) {
+            showAlertSuccess();
+        } else {
+            showAlertError();
+        }
+    });
+});
